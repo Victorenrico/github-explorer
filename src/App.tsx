388e186c061a02f00103896645c942738cc7dd60
Victor Codonho <victorenrico50@gmail.com>
2021-01-24 15:22:12 -0300

    Criando Rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
-function App() {
-    return <div className="App" />;
-}
+import Routes from './routes';
+
+const App: React.FC = () => {
+    return (
+        <BrowserRouter>
+            <Routes />
+        </BrowserRouter>
+    );
+};
 
 export default App;
