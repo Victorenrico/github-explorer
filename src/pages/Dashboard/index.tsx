fbcc166127200d6e761fb0fa0b1feda04cdfd157
Victor Codonho <victorenrico50@gmail.com>
2021-01-24 16:38:05 -0300

    Estilizando Dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,37 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Title } from './styles';
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-    return <Title>Explore repositórios no Github</Title>;
+    return (
+        <>
+            <img src={logoImg} alt="Github Explorer" />
+            <Title>Explore repositórios no Github</Title>
+
+            <Form action="">
+                <input placeholder="Digite o nome do repositório" />
+                <button type="submit">Pesquisar</button>
+            </Form>
+
+            <Repositories>
+                <a href="teste">
+                    <img
+                      src="https://avatars.githubusercontent.com/u/26679579?s=460&u=788047854a1e06b47b527b384bac3f59f1e5c26b&v=4"
+                      alt="Victor Codonho"
+                    />
+                    <div>
+                        <strong>rocketseat/unform</strong>
+                        <p>Teste</p>
+                    </div>
+
+                    <FiChevronRight size={20} />
+                </a>
+            </Repositories>
+        </>
+    );
 };
 
 export default Dashboard;
